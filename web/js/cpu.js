418297df4e7c87d95d8e85dc807a834c25fe0bb0
Matthew Abtin <staalkoper@gmail.com>
2020-07-15 11:10:58 +0200

    Changed to external files

--- web/js/cpu.js
+++ web/js/cpu.js
@@ -1,19 +1,36 @@
 
-//  Calls the python function to get the cpu brand
-const getCpuBrand = () => eel.getCpuBrand() ((dataValue) => {
-    document.getElementById("cpuBrand").innerHTML=dataValue;
-});
 
-// Calls the python function to get the cpu core count
-const getCpuCores = () => eel.getCpuCores() ((dataValue) => {
-    document.getElementById("cpuCores").innerHTML=dataValue;
-});
+class cpu {
 
-// Calls the function to get the cpu thread count
-const getCpuThreads = () => eel.getCpuThreads() ((dataValue) => {
-    document.getElementById("cpuThreads").innerHTML=dataValue;
-});
+    //  Calls the python function to get the cpu brand
+    Brand = () => eel.getCpuBrand() ((dataValue) => {
+        document.getElementById("cpuBrand").innerHTML=dataValue;
+    });
 
-const getCpuFrequency = () => eel.getCpuFrequency() ((dataValue) => {
-    document.getElementById("cpuFrequency").innerHTML=dataValue
-});
+    // Calls the python function to get the cpu core count
+    Cores = () => eel.getCpuCores() ((dataValue) => {
+        document.getElementById("cpuCores").innerHTML=dataValue;
+    });
+
+    // Calls the function to get the cpu thread count
+    Threads = () => eel.getCpuThreads() ((dataValue) => {
+        document.getElementById("cpuThreads").innerHTML=dataValue;
+    });
+
+    // Calls the function to get the cpu frequency
+    Frequency = () => eel.getCpuFrequency() ((dataValue) => {
+        document.getElementById("cpuFrequency").innerHTML=dataValue
+    });
+
+}
+
+window.onload = function() {
+    // Init the class
+    Cpu = new cpu()
+
+    // Cpu stuff
+    Cpu.Brand()
+    Cpu.Cores()
+    Cpu.Threads()
+    Cpu.Frequency()
+}
