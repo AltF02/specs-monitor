5c771d7c7e4b8c4afce442e6bb124931ec06228c
Matthew Abtin <staalkoper@gmail.com>
2020-07-15 10:38:07 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,33 @@
+"""
+This is a simple gui that gets your specs.
+I wanted to use classes but that is impossible with eel
+"""
+
+import eel
+import cpuinfo
+import psutil
+
+eel.init('web')
+
+info = cpuinfo.get_cpu_info()
+
+# Gets the cpu brand
+@eel.expose
+def getCpuBrand():
+    return info["brand_raw"].replace("(R)", "®")
+
+@eel.expose
+def getCpuCores():
+    return psutil.cpu_count(logical=False)
+
+@eel.expose
+def getCpuThreads():
+    return psutil.cpu_count()
+
+@eel.expose
+def getCpuFrequency():
+    return info["hz_actual_friendly"]
+
+eel.start('main.html')
+while True:
+    eel.sleep(10)
